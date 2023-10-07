fc5d9c78e1b0df2305fa291e6066f1718676cc5e
unknown <dragotoiug@gmail.com>
2023-10-07 13:32:47 +0300

    Small changes to the screenshots and support

--- visual-tests/test-vizual.spec.ts
+++ visual-tests/test-vizual.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Verificare vizuala pagina dashboard', async ({ page }) => {
-  await page.goto('https://ui.shadcn.com/examples/dashboard')
+ // await page.setViewportSize({
+  //  width: 1920,
+  //  height: 1080,
+  // });
+  await page.goto('https://ui.shadcn.com/examples/cards')
+  expect(page.isVisible).toBeTruthy
   await expect(page).toHaveScreenshot('reference-screenshot.png')
 
 
